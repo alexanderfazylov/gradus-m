defc14360fffa386da80005358a475647166f4fb
Maks Gryaznov <lkdnvc@gmail.com>
2013-08-26 16:39:17 +0400

    go home

--- protected/modules/userAdmin/views/admin/info.php
+++ protected/modules/userAdmin/views/admin/info.php
@@ -1,70 +1,107 @@
 <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/fileuploader.js"></script>
-<script>
-    var base_url = "<?php echo $this->createAbsoluteUrl('/'); ?>";
-</script>
 
-<h1>Создание описания <b><?php echo $model->name; ?></b></h1>
-<h2>Загрузка изображения на сервер</h2>
-<div class="uploded_file_">
-    <div id="download_file">
-        <noscript>
-            <p>Включите JavaScript чтобы испльзовать file uploader.</p>
-        </noscript>
-    </div>
-</div>
-<ul class="uload_list_img">
-</ul>
-<div class="anchor"></div>
-<h2>Редактирвание текста</h2>
-<script>
-    var uploader = new qq.FileUploader({
-        element: document.getElementById('download_file'),
-        multiple: false,
-        action: '/userAdmin/admin/downloadImg',
-        debug: false,
-        onSubmit: function (id, fileName) {
-            //
-        },
-        onComplete: function (id, fileName, responseText) {
-            var now_url = base_url+'/uploads/'+responseText.filename;
-            $('.uload_list_img').append('<li><input type="text" value="'+now_url+'"></li>');
 
-        }
-    });
-</script>
+<form id="sample" class="classic_form">
+    <label>
+        <span>Адрсе ссылки логотипа продукта</span>
+        <input type="text" value="" placeholder="http://adres.ru"/>
+    </label>
+    <label>
+        <span>Картинка логотипа продукта</span>
 
-<div id="sample">
-    <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/nicEdit-latest.js"></script>
-    <script type="text/javascript">
-        //<![CDATA[
-        bkLib.onDomLoaded(function () {
-            nicEditors.allTextAreas()
-        });
-        //]]>
-    </script>
-    <textarea id='my_text' name="Equipment[text]" style="width: 100%;min-height: 400px">
-        <?php echo $model->text; ?>
-    </textarea><br/>
-    <br/>
-    <input type="submit" value="Сохранить" onclick="saveEqText(<?php echo $model->id; ?>);return false"/>
-
-    <script>
-        function saveEqText(id) {
-            $.ajax({
-                type: "POST",
-                url: '/userAdmin/admin/saveEqText?id=' + id,
-                dataType: "json",
-                data: ({
-                    'text': $('.nicEdit-main').html()
-                }),
-                success: function (data) {
-                    if (data == null) {
-                        alert('Ошибка. Попробуйте перезагрузить страницу.');
-                    } else {
-                        alert('Сохранено');
-                    }
-
+        <div class="uploded_file">
+            <div id="download_file_file_id_mini">
+                <noscript>
+                    <p>Включите JavaScript чтобы испльзовать file uploader.</p>
+                </noscript>
+            </div>
+        </div>
+        <script>
+            var uploader = new qq.FileUploader({
+                element: document.getElementById('download_file_file_id_mini'),
+                multiple: false,
+                action: '/userAdmin/admin/downloadImg',
+                debug: false,
+                onSubmit: function (id, fileName) {
+                    //
+                },
+                onComplete: function (id, fileName, responseText) {
+                    //
                 }
             });
-        }
-    </script>
+        </script>
+    </label>
+    <label>
+        <span>Описание продукта</span>
+
+        <p>Для создания стилизованного списка, используйте разметку:</p>
+
+        <p>&#60;ul&#62;<br/>
+            &#60;li&#62;Первый элемент&#60;/li&#62;<br/>
+            &#60;li&#62;Второй элемент&#60;/li&#62;<br/>
+            &#60;li&#62;Третий элемент&#60;/li&#62;<br/>
+            &#60;/ul&#62;
+
+            <br/>
+            <br/>
+            Для выделениея абзаца, используйте разметку
+            <br/>&#60;p&#62;Абзац&#60;/p&#62;
+            <textarea id='my_text' name="Equipment[text]" style="width: 100%;min-height: 400px">
+                <?php echo $model->text; ?>
+            </textarea>
+        </p>
+    </label>
+    <label>
+        <span>Картинка внизу под текстом</span>
+
+        <div class="uploded_file">
+            <div id="uploded_file_file_id_img">
+                <noscript>
+                    <p>Включите JavaScript чтобы испльзовать file uploader.</p>
+                </noscript>
+            </div>
+        </div>
+        <script>
+            var uploader = new qq.FileUploader({
+                element: document.getElementById('uploded_file_file_id_img'),
+                multiple: false,
+                action: '/userAdmin/admin/downloadImg',
+                debug: false,
+                onSubmit: function (id, fileName) {
+                    //
+                },
+                onComplete: function (id, fileName, responseText) {
+                    //
+                }
+            });
+        </script>
+    </label>
+
+    <input type="submit" value="Сохранить" onclick="saveEqText(<?php echo $model->id; ?>);return false"/>
+</form>
+<script>
+    function saveEqText(id) {
+        $.ajax({
+            type: "POST",
+            url: '/userAdmin/admin/saveEqText?id=' + id,
+            dataType: "json",
+            data: ({
+                'text': $('.nicEdit-main').html()
+            }),
+            success: function (data) {
+                if (data == null) {
+                    alert('Ошибка. Попробуйте перезагрузить страницу.');
+                } else {
+                    alert('Сохранено');
+                }
+
+            }
+        });
+    }
+</script>
+<style>
+    .uploded_file {
+        display: inline-block !important;
+        margin-left: 20px !important;
+    }
+</style>
