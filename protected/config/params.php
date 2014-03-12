3ddd1a21ade470d6823055a808b9d049ab68e384
alexanderfazylov <alexander@fazylov.ru>
2014-03-12 11:29:12 +0300

    Update params.php

--- protected/config/params.php
+++ protected/config/params.php
@@ -6,6 +6,6 @@ return array(
         "host" => "smtp.yandex.ru",
         "user" => "atpp.16mb@yandex.ru",
         "password" => "7a8a7a8a",
-        "eventer" => "alexander@fazylov.ru"
+        "eventer" => "info@gradus-m.ru"
     )
 );
